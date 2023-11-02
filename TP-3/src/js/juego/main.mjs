96c40d09a8db7a8ea0a6e7c55faf5db8342df5c5
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-02 17:44:30 -0300

    Drag de fichas (sin restricciones de pertenencia de la ficha con el jugador)

--- TP-3/src/js/juego/main.mjs
+++ TP-3/src/js/juego/main.mjs
@@ -1,11 +1,9 @@
 import Tablero from "./Clases/Tablero.mjs";
-import Jugador from "./Clases/Jugador.mjs";
 
 const CUATRO_L = "4";
 const CINCO_L = "5"; 
 const SEIS_L = "6";
 const SIETE_L = "7";
-let cont_jugador = 0;
 
 let canvas = document.querySelector("#canvas_juego");
 let canvas_context = canvas.getContext("2d");
@@ -27,6 +25,59 @@ btn_jugar.addEventListener("click", () => {
 let formulario = document.querySelector("form");
 formulario.addEventListener("submit", (e) => {
 	e.preventDefault();
+	
+	let botonIzquierdoClickeado = false;
+	
+	function detectarClick(event){
+		botonIzquierdoClickeado = true;
+		// console.log("Mouse Down!" + botonIzquierdoClickeado);
+		
+		for(let i = tablero_juego.fichas.length-1;i >= 0;i--){
+			// Se clickea el boton del mouse y además este se está dentro de la ficha actual 
+			if(botonIzquierdoClickeado && tablero_juego.mouseDentroDeLaFicha(i, event)){
+				// Seteamos la ficha como clickeada
+				tablero_juego.setearFichaComoClickeada(i);
+				// Le asignamos las nuevas coordenadas (centra la ficha con respecto al mouse)
+				tablero_juego.setearNuevasCoordenadasAFicha(i, event.offsetX, event.offsetY);
+				return; // A la primera ficha que cumpla cortamos el bucle
+			}
+		}
+	}
+	
+
+	function deseleccionarFichas(){
+		botonIzquierdoClickeado = false;
+		// console.log("Mouse Up!" + botonIzquierdoClickeado);
+		
+		// Se desclickean todas las fichas
+		for(let i = tablero_juego.fichas.length-1;i >= 0;i--){
+			tablero_juego.setearFichaComoDesclickeada(i);
+		}
+	}
+	
+	
+	function moverFicha(event){
+		for(let i = tablero_juego.fichas.length-1;i >= 0;i--){
+			// Se verifica si la ficha tiene su estado "clickeada" y ademas el mouse está dentro de la ficha 
+			if(tablero_juego.fichaEstaSiendoClickeada(i, event)){
+				// Seteamos las nuevas coordenadas de la ficha
+				tablero_juego.setearNuevasCoordenadasAFicha(i, event.offsetX, event.offsetY);
+			}
+		}
+	}
+	
+	canvas.addEventListener("mousedown", detectarClick);
+	canvas.addEventListener("mouseup", deseleccionarFichas);
+	
+	canvas.addEventListener("click", ()=>{
+		console.log("Click");
+	});
+	
+	canvas.addEventListener("mouseleave", deseleccionarFichas);
+	canvas.addEventListener("mousemove", moverFicha);
+
+
+
 	const dataForm = new FormData(formulario);
 
 	let opctablero = dataForm.get("tablero");
@@ -48,8 +99,13 @@ formulario.addEventListener("submit", (e) => {
 	// Creeamos un tablero centrado en el cambas
 	const tablero_juego = new Tablero(canvas_context, opctablero, desafio, jugador1, jugador2);
 	
-	tablero_juego.dibujar();
 
+
+	function ejecutarJuego(){	
+		tablero_juego.dibujar();
+	}
+
+	setInterval(ejecutarJuego, 10);
 });
 
 
