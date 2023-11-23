70d083d9847d42441b023423431b641b93216f3e
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-23 16:17:41 -0300

    ADD parallax del mouse

---
+++ TP-4/parallax-mouse.js
@@ -0,0 +1,27 @@
+const wrapper = document.getElementById("wrapper-paralax-mouse");
+const images = document.querySelectorAll("#wrapper-paralax-mouse img");
+
+wrapper.addEventListener("mousemove", (e) =>{
+  let centro_x = 1080 / 2;
+  let centro_y = 530 / 2;
+  let mouse_x = e.offsetX;
+  let mouse_y = e.offsetY;
+  
+  let desplazamiento_cielo = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
+  let desplazamiento_edificios = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
+  let desplazamiento_personajes = [`${-(mouse_x - centro_x) * 0.01}%`, `${-(mouse_y - centro_y) * 0.01}%`];
+  
+  let x = `${desplazamiento_cielo}, ${desplazamiento_edificios}`;
+  
+  // No nos toma que al hacer hover nos mueva las imagenes del fondo.
+  wrapper.style.backgroundPosition = x;
+
+  images.item(0).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+  images.item(1).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+  images.item(2).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+});
+
+function cambiarPorcentajeDeTransform(imagen, event){
+
+}
+
