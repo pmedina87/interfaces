37f92589f1ea209851ea3ac2f6cd8390ea3f03f1
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-24 18:55:19 -0300

    Efecto menu cerrar

--- TP-4/js/header.js
+++ TP-4/js/header.js
@@ -1,39 +1,39 @@
 const boton = document.getElementById("btn-hamburguesa");
 const raya_centro = document.querySelector("#btn-hamburguesa div");
-const elems = [];
+const elems1 = [];
+const elems2 = [];
 const elementos_nav = document.querySelectorAll("#navbar li").forEach(elem => {
-  elems.push(elem);
+  elems1.push(elem);
 });
 
 
 boton.addEventListener("click", () => {
   boton.classList.toggle("btn-click");
-  
-  // Verificamos que los elementos sean mayor a 0
-  if(elems.length > 0){
-    // Quitamos el primer elemento del arreglo y lo pasamos a la funcion 
-    toggleClass(elems.shift());
+  if(elems1.length > 0){
+    elems1.map(elem => elems2.push(elem));
+    toggleClass(elems1);
+  }else{
+    elems2.map(elem => elems1.push(elem));
+    elems2.reverse();
+    toggleClass(elems2);
   }
 });
 
-// """"Recursividad""""
-function toggleClass(elem){
+// Recursividad
+function toggleClass(lista){
+  // Verificamos que los elementos sean mayor a 0
+  if(lista.length <= 0){ return ;}
+
   // Creamos una promesa
   let a = new Promise((resolve, reject) => {
     // Luego de 300ms agregara/quitará la clase
     setTimeout(() => {
-      elem.classList.toggle("showElement");
+      lista.shift().classList.toggle("showElement");
       // Damos como terminada la promesa
       resolve();
     }, 300);
   });
- 
+  
   // Una vez la promesa este resuelta ejecutamos lo siguiente 
-  a.then(() => {
-    // Verificamos nuevamente que el arreglo de elementos tenga elementos
-    if(elems.length > 0 ){
-      // Volvemos a llamar a la funcion con el primer elemento del arreglo
-      toggleClass(elems.shift());
-    }
-  });
+  a.then(() => toggleClass(lista));
 }
