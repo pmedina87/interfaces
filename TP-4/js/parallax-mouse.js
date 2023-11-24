e1c030218542b689e542b911cf5417d5e53c158b
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-23 21:53:45 -0300

    Transicion de imagenes en seccion descripcion

--- TP-4/js/parallax-mouse.js
+++ TP-4/js/parallax-mouse.js
@@ -1,22 +1,24 @@
-const wrapper = document.getElementById("wrapper-paralax-mouse");
-const images = document.querySelectorAll("#wrapper-paralax-mouse img");
-
-wrapper.addEventListener("mousemove", (e) =>{
-  let centro_x = 1080 / 2;
-  let centro_y = 530 / 2;
-  let mouse_x = e.offsetX;
-  let mouse_y = e.offsetY;
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.getElementById("wrapper-paralax-mouse");
+  const images = document.querySelectorAll("#wrapper-paralax-mouse img");
   
-  let desplazamiento_cielo = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
-  let desplazamiento_edificios = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
-  let desplazamiento_personajes = [`${-(mouse_x - centro_x) * 0.01}%`, `${-(mouse_y - centro_y) * 0.01}%`];
-  
-  let x = `${desplazamiento_cielo}, ${desplazamiento_edificios}`;
-  
-  // No nos toma que al hacer hover nos mueva las imagenes del fondo.
-  wrapper.style.backgroundPosition = x;
-
-  images.item(0).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
-  images.item(1).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
-  images.item(2).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+  wrapper.addEventListener("mousemove", (e) =>{
+    let centro_x = 1080 / 2;
+    let centro_y = 530 / 2;
+    let mouse_x = e.offsetX;
+    let mouse_y = e.offsetY;
+    
+    let desplazamiento_cielo = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
+    let desplazamiento_edificios = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
+    let desplazamiento_personajes = [`${-(mouse_x - centro_x) * 0.01}%`, `${-(mouse_y - centro_y) * 0.01}%`];
+    
+    let x = `${desplazamiento_cielo}, ${desplazamiento_edificios}`;
+    
+    // No nos toma que al hacer hover nos mueva las imagenes del fondo.
+    wrapper.style.backgroundPosition = x;
+    
+    images.item(0).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+    images.item(1).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+    images.item(2).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+  });
 });
--- TP-4/js/seccion_descripciones.js
+++ TP-4/js/seccion_descripciones.js
@@ -4,16 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
 	let tituloB = document.querySelector("#tituloB");
 	let tituloC = document.querySelector("#tituloC");
 	let tituloD = document.querySelector("#tituloD");
-
-    let arrTitulos = [tituloA, tituloB, tituloC, tituloD];
+  let arrTitulos = [tituloA, tituloB, tituloC, tituloD];
 
 	let textoA = document.querySelector("#textoA");
 	let textoB = document.querySelector("#textoB");
 	let textoC = document.querySelector("#textoC");
 	let textoD = document.querySelector("#textoD");
-
 	let arrTextos = [textoA, textoB, textoC, textoD];
-    // obtenemos las imagenes que tenemos que ir cambiando
+
+  // obtenemos las imagenes que tenemos que ir cambiando
 	let imgA = document.querySelector("#imgA");
 	let imgB = document.querySelector("#imgB");
 	let imgC = document.querySelector("#imgC");
@@ -37,39 +36,10 @@ document.addEventListener("DOMContentLoaded", function () {
 				arrTextos.forEach((title) => (title.style.opacity = 0));
 
 				arrTitulos.forEach((title) => (title.style.opacity = 0));
-
-				//segun la pos en el arreglo sera la imagen y texto q quiero ver
-				switch (index) {
-					case 0:
-						estilos("A");
-						break;
-					case 1:
-                        estilos("B");
-						// imgB.style.opacity = 1;
-						// textoA.style.opacity = 0;
-						// tituloA.style.opacity = 0;
-						// textoB.style.opacity = 1;
-						// tituloB.style.opacity = 1;
-						break;
-					case 2:
-                        estilos("C");
-						// imgC.style.opacity = 1;
-						// textoC.style.opacity = 1;
-						// title3.style.opacity = 1;
-						// text2.style.opacity = 0;
-						// title2.style.opacity = 0;
-						break;
-					case 3:
-                        estilos("D");
-						// img4.style.opacity = 1;
-						// title4.style.opacity = 1;
-						// text3.style.opacity = 0;
-						// title3.style.opacity = 0;
-						// text4.style.opacity = 1;
-						break;
-					default:
-						break;
-				}
+				
+				arrImg[index].style.opacity = 1;
+				arrTextos[index].style.opacity = 1;
+				arrTitulos[index].style.opacity = 1;
 			}
 		});
 	});
@@ -78,13 +48,4 @@ document.addEventListener("DOMContentLoaded", function () {
 	arrTextos.forEach((text) => {
 		observador.observe(text);
 	});
-
-    function estilos(tipo) {
-        let imagen =  "img" + tipo;
-        let texto =  "texto" + tipo;
-        let titulo =  "titulo" + tipo;
-        imagen.style.opacity = 1;
-		texto.style.opacity = 1;
-		titulo.style.opacity = 1;
-    };
 });
