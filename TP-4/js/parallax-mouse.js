83cdc983e4d4cf45a4333891122ea91ee4e9cc4f
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-24 16:29:23 -0300

    Refactor parallax mouse

--- TP-4/js/parallax-mouse.js
+++ TP-4/js/parallax-mouse.js
@@ -10,15 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
     
     let desplazamiento_cielo = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
     let desplazamiento_edificios = `${50 - (e.clientX - centro_x) * 0.01}% ${50 - (e.clientY - centro_y) * 0.01}%`;
-    let desplazamiento_personajes = [`${-(mouse_x - centro_x) * 0.01}%`, `${-(mouse_y - centro_y) * 0.01}%`];
+    // Sigue el movimiento del mouse
+    let desplazamiento_pj_izquierda = [`${(mouse_x - centro_x) * 0.03}%`, `${(mouse_y - centro_y) * 0.03}%`];
+    // Va en contra del movimiento del mouse
+    let desplazamiento_pj_centro = [`${-(mouse_x - centro_x) * 0.04}%`, `${-(mouse_y - centro_y) * 0.04}%`];
+    let desplazamiento_pj_derecha = [`${(mouse_x - centro_x) * 0.005}%`, `${(mouse_y - centro_y) * 0.005}%`];
     
     let x = `${desplazamiento_cielo}, ${desplazamiento_edificios}`;
     
     // No nos toma que al hacer hover nos mueva las imagenes del fondo.
     wrapper.style.backgroundPosition = x;
     
-    images.item(0).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
-    images.item(1).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
-    images.item(2).style.transform = `translate(${desplazamiento_personajes[0]}, ${desplazamiento_personajes[1]})`;
+    images.item(0).style.transform = `translate(${desplazamiento_pj_izquierda[0]}, ${desplazamiento_pj_izquierda[1]})`;
+    // Sigue el movimiento del mouse pero en diagonal
+    images.item(1).style.transform = `translate(${desplazamiento_pj_derecha[1]}, ${desplazamiento_pj_derecha[0]})`;
+    images.item(2).style.transform = `translate(${desplazamiento_pj_centro[0]}, ${desplazamiento_pj_centro[1]})`;
   });
 });
