24408c60e7700c6b7442fb7f657da08a113340e4
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-27 00:59:06 -0300

    ADD: Transicion provisoria de personajes en el segundo parallax

--- TP-4/js/parallax-mouse.js
+++ TP-4/js/parallax-mouse.js
@@ -39,4 +39,34 @@ document.addEventListener("DOMContentLoaded", function () {
     images.item(1).style.transform = `translate(${desplazamiento_pj_derecha[1]}, ${desplazamiento_pj_derecha[0]})`;
     images.item(2).style.transform = `translate(${desplazamiento_pj_centro[0]}, ${desplazamiento_pj_centro[1]})`;
   });
+  
+  /* Animamos una entrada de los personajes para cuando el container esté a la viste del usuario */
+  const personajes = [];
+  document.querySelectorAll(".paralax-container img").forEach(personaje => personajes.push(personaje));
+  personajes.pop(); // Eliminamos la imagen del pasto 
+  // Instanciamos un observador el cual va a verificar si las "entries" aparecen en pantalla.
+  const observador = new IntersectionObserver(entries => {
+    // Aunque sea 1 solo elemento, las entradas deben ser un arreglo.
+    entries.forEach(entry => {
+      //esta visible?
+      if (entry.intersectionRatio) {
+        // Agregar efecto de entrada
+        wrapper.classList.add("visible");
+        console.log("Es visible");
+      }else{
+        // Al hacer hover en el wrapper le seteamos style="transform" a cada imagen,
+        // Y un estilo directamente setteado en html tiene, en terminos de cascada, mayor importancia.
+        // Esto nos impedia realizar la animacion de esconder los personajes cuando el contenedor no estaba visible.
+        // 2 horas y era esto...
+        personajes.forEach(personaje => personaje.removeAttribute("style"));
+
+        // Rollback al efecto de entrada
+        wrapper.classList.remove("visible");
+        console.log("NO es visible");
+      }
+    })
+  });
+
+  //Le indicamos al observador que checkee el container
+  observador.observe(wrapper);
 });
