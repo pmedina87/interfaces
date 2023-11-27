c0262ced04e625464de50282e0c55cb23618ef16
renzo-guerra <renzoguerralatt@gmail.com>
2023-11-27 20:44:54 -0300

    Cambio tiempo al loader inicial

--- TP-4/js/index.js
+++ TP-4/js/index.js
@@ -3,13 +3,13 @@
  */
 
 document.addEventListener("DOMContentLoaded", function() {
-    // Simula un retraso de carga de 2 segundos (puedes cambiar este valor)
+    // Simula un retraso de carga de 5 segundos (puedes cambiar este valor)
   setTimeout(function() {
       // Oculta la barra de carga
       document.querySelector(".loader-container").style.display = "none";
       // Muestra el contenido de la página
       document.getElementById("content").style.display = "block";
-  }, 1000); // 2000 milisegundos (2 segundos)
+  }, 5000); // 5000 milisegundos (2 segundos)
 
   // Simula el progreso de carga de 0 a 100
   let progressBar = document.getElementById("progress-bar");
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
           // Cambia el color de la barra a medida que avanza el progreso
           progressBar.style.backgroundColor = `hsl(${width}, 50%, 50%)`;
       }
-  }, 1); // Puedes ajustar la velocidad del progreso aquí
+  }, 45); // Puedes ajustar la velocidad del progreso aquí
 });
 
 const blackSpider = document.getElementById('blackSpider');
